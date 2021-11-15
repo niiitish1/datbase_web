d542580c7469aaa15f4d221fd821d151fbe440a7
niiitish1 <71102495+niiitish1@users.noreply.github.com>
2021-11-15 12:51:08 +0530

    big change

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,9 @@ import 'dart:convert';
 
 import 'package:database_app/add_user.dart';
 import 'package:database_app/user_data_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
@@ -27,82 +29,84 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, indexNo) {
-          var userData = arr[indexNo];
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                UserDataModel data = UserDataModel(userData["name"],
-                    userData["email"], userData["gender"], userData["status"]);
-                return AddUser(model: data);
-              }));
-            },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Id: ${userData['id'].toString()}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    // color: Colors.w,
-                  ),
-                ),
-                Text(
-                  'Name: ${userData['name']}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    // color: Colors.white,
-                  ),
-                ),
-                Text(
-                  'Email: ${userData['email']}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    // color: Colors.white,
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "gender: ${userData["gender"]}",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    userData["gender"] == "male"
-                        ? Icon(Icons.male)
-                        : Icon(Icons.female)
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Status: ${userData['status']}',
-                      style: TextStyle(
-                        fontSize: 16,
-                        // color: Colors.white,
+      appBar: AppBar(
+        title: Text("MyApp"),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, indexNo) {
+            var userData = arr[indexNo];
+            return Container(
+              padding: EdgeInsets.all(8),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                Expanded(
+                  flex: 7,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildTextSpan("ID: ", userData["id"].toString()),
+                      _buildTextSpan("Name: ", userData["name"]),
+                      GestureDetector(
+                          onTap: () {
+                            Fluttertoast.showToast(msg: "Opening Gmail");
+                          },
+                          child: _buildTextSpan("Email: ", userData["email"])),
+                      Row(
+                        children: [
+                          _buildTextSpan("Gender: ", userData["gender"]),
+                          userData["gender"] == "male"
+                              ? Icon(
+                                  Icons.male,
+                                  color: Colors.red,
+                                )
+                              : Icon(
+                                  Icons.female,
+                                  color: Colors.green,
+                                )
+                        ],
                       ),
-                    ),
-                    //teermaeri
-                    userData["status"] == "active"
-                        ? Container(
-                            width: 10,
-                            height: 25,
-                            color: Colors.green,
-                          )
-                        : Container(
-                            width: 10,
-                            height: 25,
-                            color: Colors.red,
-                          )
-                  ],
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          _buildTextSpan("Status: ", userData["status"]),
+                          SizedBox(width: 10),
+                          userData["status"] == "active"
+                              ? Container(
+                                  height: 7,
+                                  width: 7,
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                )
+                              : Container(
+                                  height: 7,
+                                  width: 7,
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          );
-        },
-        itemCount: arr.length,
+                Expanded(
+                  child: Icon(Icons.delete),
+                ),
+                SizedBox(
+                  height: 5,
+                )
+              ]),
+            );
+          },
+          itemCount: arr.length,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -121,6 +125,31 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget _buildTextSpan(String text, var userData) {
+    return RichText(
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      text: TextSpan(
+        text: text,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+        ),
+        children: [
+          TextSpan(
+            text: userData,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 14,
+              fontWeight: FontWeight.normal,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   apiCall() async {
     var recp = await http.get(
       Uri.parse('https://gorest.co.in/public/v1/users'),
@@ -129,5 +158,79 @@ class _HomeState extends State<Home> {
     setState(() {
       arr = jsonResp['data'];
     });
+  }
+
+  Widget _showData(var userData) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Id: ${userData['id'].toString()}',
+          style: TextStyle(
+            fontSize: 16,
+            // color: Colors.w,
+          ),
+        ),
+        Text(
+          'Name: ${userData['name']}',
+          style: TextStyle(
+            fontSize: 16,
+            // color: Colors.white,
+          ),
+        ),
+        Text(
+          'Email: ${userData['email']}',
+          style: TextStyle(
+            fontSize: 16,
+            // color: Colors.white,
+          ),
+        ),
+        Row(
+          children: [
+            Text(
+              "gender: ${userData["gender"]}",
+              style: TextStyle(fontSize: 16),
+            ),
+            userData["gender"] == "male" ? Icon(Icons.male) : Icon(Icons.female)
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Status: ${userData['status']}',
+              style: TextStyle(
+                fontSize: 16,
+                // color: Colors.white,
+              ),
+            ),
+            //teermaeri
+            userData["status"] == "active"
+                ? Container(
+                    width: 10,
+                    height: 25,
+                    color: Colors.green,
+                  )
+                : Container(
+                    width: 10,
+                    height: 25,
+                    color: Colors.red,
+                  )
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _dectectClick(var userData) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          UserDataModel data = UserDataModel(userData["name"],
+              userData["email"], userData["gender"], userData["status"]);
+          return AddUser(model: data);
+        }));
+      },
+    );
   }
 }
